1020636b2a0689de4fb61a9337ca10e5a1a43062
Stormev <pidala1990@gmail.com>
2024-07-21 19:45:47 +0300

    0.1 - SCREEN RATIO UPDATE

--- client/main.py
+++ client/main.py
@@ -12,13 +12,31 @@ from pyautogui import prompt
 user32 = windll.user32
 # Ставим размер окна под разрешение монитора пользователя
 user_monitor = (user32.GetSystemMetrics(0), user32.GetSystemMetrics(1))
-WIDTH, HEIGHT = user_monitor[0], user_monitor[1]  # 1920, 1080
+WIDTH, HEIGHT = user_monitor[0], user_monitor[1]
+
+# test
+#WIDTH, HEIGHT = 1600 × 900
+
+# Стандартная разность между длиной и высотой
+# Для измениния объектов в соотношение размера экрана
+RATIO = 700
+# Процент отклонения от стандартного разрешения экрана
+SCREEN_RATIO_MODIFICATION = (WIDTH - HEIGHT) / RATIO
+
+# Полный экран
 isFullScreen = False
 
+# Инициация библиотеки
 pygame.init()
 pygame.display.set_caption('Subbiz')
 clock = pygame.time.Clock()
-main_font = pygame.font.Font(None, 40)
+
+# Настройка текста
+font_size = 40
+font_size = int(font_size * SCREEN_RATIO_MODIFICATION)
+main_font = pygame.font.Font(None, font_size)
+
+# окно
 screen = pygame.display.set_mode((WIDTH, HEIGHT), (pygame.FULLSCREEN if isFullScreen else pygame.NOFRAME))
 if isFullScreen:
     WIDTH, HEIGHT = screen.get_size()
@@ -31,17 +49,109 @@ def loader_image(path):
     return pygame.image.load(path)
 
 
-class Button(pygame.sprite.Sprite):
-    def __init__(self, pos=(0, 0), func=None, scale=1):
-        super(ButtonEdit, self).__init__()
+# Класс таблички где display - рабочий экран, primary_image - изначальное изображение
+# pos - позиция на экране
+# text - изначальный текст
+# scale - модификатор размера
+# color - цвет текста
+
+
+class Label(pygame.sprite.Sprite):
+    def __init__(self, display, pos=(0, 0), scale=1, text='', color=(255, 255, 255)):
+        super().__init__()
+        self.screen = display
+
         self.image = None
-        self.rect = self.active.get_rect()
+        self.rect = None
+
+        self.position = pos
+        self.scale = scale
+        self.text = text
+        self.color = color
+
+        self.image_change(loader_image('data/UI/label.png'))
+
+    def image_change(self, new_image):
+        self.image = pygame.transform.scale(new_image, (
+            new_image.get_size()[0] * self.scale * SCREEN_RATIO_MODIFICATION,
+            new_image.get_size()[1] * self.scale * SCREEN_RATIO_MODIFICATION))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = self.position[0], self.position[1]
+
+    def update(self, events):
+        self.screen.blit(main_font.render(self.text, False, self.color),
+                         (self.position[0] + self.image.get_size()[0]//3,
+                          self.position[1] + self.image.get_size()[1]//2.25))
+
+
+# Класс кнопки где display - рабочий экран, primary_image - изначальное изображение(если кнопки не будет иметь OnHover)
+# OnHover:
+# image_inactive - изображение деактивированной кнопки, image_active- изображение ктивированной кнопки
+# pos - позиция на экране
+# func - исполняемая функция при нажатии
+# scale - модификатор размера
+
+class Button(pygame.sprite.Sprite):
+    def __init__(self, display, primary_image, image_inactive=None, image_active=None, pos=(0, 0), func=None, scale=1):
+        super(Button, self).__init__()
+        self.screen = display
+
+        self.primary_image = primary_image
+        self.primary_image = loader_image(primary_image) if type(primary_image) == str else None
+
+        self.image = None
+        self.rect = None
+
+        self.image_inactive = image_inactive
+        if image_inactive and type(image_inactive) == str:
+            self.image_inactive = loader_image(image_inactive)
+
+        self.image_active = image_active
+        if image_active and type(image_active) == str:
+            self.image_active = loader_image(image_active)
+
+        self.position = pos
         self.scale = scale
         self.func = func
 
-    def image_change(self, class_image):
-        self.image = pygame.transform.scale(class_image, (class_image.get_size()[0] * self.scale,
-                                                          class_image.get_size()[1] * self.scale))
+        self.image_change(primary_image)
+
+    def image_change(self, new_image):
+        self.image = pygame.transform.scale(new_image, (
+            new_image.get_size()[0] * self.scale * SCREEN_RATIO_MODIFICATION,
+            new_image.get_size()[1] * self.scale * SCREEN_RATIO_MODIFICATION))
+        self.rect = self.image.get_rect()
+        self.rect.x, self.rect.y = self.position[0], self.position[1]
+
+    def update(self, events):
+        pos = pygame.mouse.get_pos()
+        hit = self.rect.collidepoint(pos)
+
+        def event_func():
+            for event in events:
+                if event.type == pygame.MOUSEBUTTONDOWN and self.func:
+                    self.func()
+
+        if hit and not (self.image_inactive or self.image_active):
+            event_func()
+        elif hit:
+            self.image_change(self.image_active)
+            event_func()
+        elif self.image_inactive:
+            self.image_change(self.image_inactive)
+
+    def get_size(self):
+        return self.image.get_size()
+
+    def get_rect(self):
+        return self.rect
+
+
+# Класс кнопки редактирования
+# display - рабочий экран
+# pos - позиция на экране
+# func - исполняемая функция при нажатии
+# scale - модификатор размера
 
 
 class ButtonEdit(pygame.sprite.Sprite):
@@ -60,8 +170,9 @@ class ButtonEdit(pygame.sprite.Sprite):
         self.func = func
 
     def image_change(self, class_image):
-        self.image = pygame.transform.scale(class_image, (class_image.get_size()[0] * self.scale,
-                                                          class_image.get_size()[1] * self.scale))
+        self.image = pygame.transform.scale(class_image, (
+            class_image.get_size()[0] * self.scale * SCREEN_RATIO_MODIFICATION,
+            class_image.get_size()[1] * self.scale * SCREEN_RATIO_MODIFICATION))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = self.position[0], self.position[1]
 
@@ -82,6 +193,15 @@ class ButtonEdit(pygame.sprite.Sprite):
             self.image_change(self.inactive)
 
 
+# Класс входных значений
+# display - рабочий экран
+# pos - позиция на экране
+# text_dialog - текст при открытии диалога
+# color - цвет текста
+# text - изначальный текст
+# scale - модификатор размера
+
+
 class InputBox(pygame.sprite.Sprite):
     def __init__(self, display, pos=(0, 0), scale=1, text='', color=(255, 255, 255), text_dialog='Введите значение'):
         super().__init__()
@@ -100,8 +220,9 @@ class InputBox(pygame.sprite.Sprite):
 
     def update_model(self):
         img = loader_image('data/UI/input_box.png')
-        self.image = pygame.transform.scale(img, (img.get_size()[0] * self.scale * 1.5,
-                                                  img.get_size()[1] * self.scale))
+        self.image = pygame.transform.scale(img, (
+            img.get_size()[0] * self.scale * 1.5 * SCREEN_RATIO_MODIFICATION,
+            img.get_size()[1] * self.scale * SCREEN_RATIO_MODIFICATION))
         self.rect = self.image.get_rect()
         self.rect.x, self.rect.y = self.position[0], self.position[1]
 
@@ -120,16 +241,13 @@ class InputBox(pygame.sprite.Sprite):
     def get_size(self):
         return self.image.get_size()
 
-    def get_image(self):
-        return self.image
-
     def get_text(self):
         return self.text
 
     def update(self, events):
         self.rect.x, self.rect.y = self.position[0], self.position[1]
         self.button_edit.position = (self.rect.x + self.image.get_size()[0] + 10,
-                                    self.rect.y + self.image.get_size()[1]//4)
+                                     self.rect.y + self.image.get_size()[1]//4)
 
         self.button_edit.draw()
         self.button_edit.update(events)
@@ -141,19 +259,27 @@ class InputBox(pygame.sprite.Sprite):
 # Главное меню
 def start_screen():
     # Ассеты
-    background = loader_image('data/UI/menu.jpg')
+    background = loader_image('data/UI/menu.png')
     background = pygame.transform.scale(background, (WIDTH, HEIGHT))
 
-    input_box = InputBox(screen, text='ttttttttgdsa')
+    button_connect = Button(screen, loader_image('data/UI/button_connect_false.png'),
+                            image_inactive='data/UI/button_connect_false.png',
+                            image_active='data/UI/button_connect_true.png',
+                            scale=0.7)
+    input_box = InputBox(screen, text='', scale=0.6)
+    label = Label(screen, text='dsadsadasd', pos=(100, 100))
 
-    #rect_start_button = (WIDTH//2 - image_start_button.get_size()[0]//2,
-                         #HEIGHT//1.2 - image_start_button.get_size()[1]//2 + 20)
-    rect_input_box = (WIDTH // 2 - input_box.get_size()[0] // 2,
-                       HEIGHT // 1.7 - input_box.get_size()[1] // 2)
-    input_box.position = rect_input_box
+    pos_start_button = (WIDTH//2 - button_connect.get_size()[0]//2,
+                        HEIGHT//1.12 - button_connect.get_size()[1]//2)
+    button_connect.position = pos_start_button
+    pos_input_box = (WIDTH // 2 - input_box.get_size()[0] // 2,
+                       HEIGHT // 1.43 - input_box.get_size()[1] // 2)
+    input_box.position = pos_input_box
 
     sprites = pygame.sprite.Group()
     sprites.add(input_box)
+    sprites.add(button_connect)
+    sprites.add(label)
 
     run = True
     while run:
